4561810ec4a901485ba979e8251fe6c4f1bab310
adityamaksare <adityamaksare@outlook.com>
2024-10-08 23:46:48 +0530

    Lecture 6

---
+++ Lecture-6-File-IO/names.py
@@ -0,0 +1,11 @@
+names = []
+
+# with - defaultly closes the file
+with open("names.txt") as file:
+    for line in  file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"hello, {name}")
+
+
---
+++ Lecture-6-File-IO/students3.py
@@ -0,0 +1,8 @@
+import csv
+
+name = input("what's your name? ")
+home = input("where's your home? ")
+
+with open("students3.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name":name, "home":home})
